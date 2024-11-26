14473b6019b51e2e9536645b4232bdcc1c1809e3
drh1983 <160263549+drh1983@users.noreply.github.com>
2024-11-26 08:20:07 -0600

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,7 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# session = get_active_session()
-cnx = st.connection("snowflake")
-session = cnx.session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -25,6 +23,8 @@ ingredients_list = st.multiselect(
     ,my_dataframe
     ,max_selections=5
 )
+
+ingredients_string = ''
 
 if ingredients_list:
 
@@ -36,7 +36,7 @@ if ingredients_list:
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
 
-st.write(my_insert_stmt)
+# st.write(my_insert_stmt)
 # st.stop()
 time_to_insert = st.button('Submit Order')
 
