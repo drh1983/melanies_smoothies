14019e8a3c74e4d3ec7ff911fbf693e56b5dfbea
drh1983 <160263549+drh1983@users.noreply.github.com>
2024-11-26 08:31:06 -0600

    Update streamlit_app.py
    fix error

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 # st.text(smoothiefroot_response)
-sf_df = st.dataframe(data=smoothiesfroot_response.json(), use_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
